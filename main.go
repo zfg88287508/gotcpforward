68a6e3f646cbfb65faf5e0e8b8669b8ab69b49df
ZhiFeng Hu <hufeng1987@gmail.com>
2017-02-12 13:08:10 +0800

    Add files to repo
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+var (
+	l string
+	r string
+)
+
+func handler(conn net.Conn, r string) {
+	client, err := net.Dial("tcp", r)
+	if err != nil {
+		fmt.Println("Dial remote failed", err)
+		return
+	}
+	fmt.Println("Connected to remote ", r)
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(client, conn)
+	}()
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(conn, client)
+	}()
+}
+func main() {
+	flag.StringVar(&l, "l", "", "listen host:port")
+	flag.StringVar(&r, "r", "", "remote host:port")
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	fmt.Println(l)
+	fmt.Println(r)
+	listener, err := net.Listen("tcp", l)
+	if err != nil {
+		fmt.Println("Failed to listen on ", l, err)
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept listener. ", err)
+			return
+		}
+		fmt.Println("Accepted connection")
+		go handler(conn, r)
+	}
+}
