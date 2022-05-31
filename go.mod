46bedbd0616c4d7269954a648472cec5e1ddadea
cnmade <hufeng1987@gmail.com>
2022-05-31 15:17:42 +0800

    更新代码配置

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/netroby/gotcpforward
 
-go 1.13
+go 1.18
--- main.go
+++ main.go
@@ -6,54 +6,72 @@ import (
 	"io"
 	"net"
 	"os"
-	"strconv"
 	"time"
 )
 
-var bufSize = 8 * 1024
-var retriesCnt = 5
-var dialTimeOut = 2 * time.Second
 var (
-	l string
-	r string
-
+	l           string
+	r           string
+	DialTimeout = 2 * time.Second
+	IdleTimeout = 2 * time.Minute
 )
 
+type IdleTimeoutConn struct {
+	Conn net.Conn
+}
+
+func (self IdleTimeoutConn) Read(buf []byte) (int, error) {
+	go self.UpdateIdleTime(self.Conn, time.Now().Add(IdleTimeout))
+	return self.Conn.Read(buf)
+}
+
+func (self IdleTimeoutConn) UpdateIdleTime(c net.Conn, t time.Time) {
+	_ = c.SetDeadline(t)
+}
+
+func (self IdleTimeoutConn) Write(buf []byte) (int, error) {
+	go self.UpdateIdleTime(self.Conn, time.Now().Add(IdleTimeout))
+	return self.Conn.Write(buf)
+}
 
 func handler(conn net.Conn, r string) {
-	var i = 0
-	for i < retriesCnt {
-		fmt.Println("Dial timeout: ", dialTimeOut)
-		fmt.Println("Dial " + strconv.Itoa(i) + " times")
-		client, err := net.DialTimeout("tcp", r, dialTimeOut)
-		if err != nil {
-			fmt.Println("Dial remote failed", err)
-			i++
-			continue
-		}
-		fmt.Println("To: Connected to remote ", r)
-		go func() {
-			defer client.Close()
-			defer conn.Close()
-			clientbuf := make([]byte, bufSize)
-			io.CopyBuffer(client, conn, clientbuf)
-		}()
-		go func() {
-			defer client.Close()
-			defer conn.Close()
-			serverbuf := make([]byte, bufSize)
-			io.CopyBuffer(conn, client, serverbuf)
-		}()
-		break
+	client, err := net.DialTimeout("tcp", r, DialTimeout)
+	if err != nil {
+		fmt.Println("Dial remote failed", err)
+
+		return
+	}
+	fmt.Println("To: Connected to remote ", r)
+
+	idleCbw := IdleTimeoutConn{
+		Conn: conn,
+	}
+	idleCbr := IdleTimeoutConn{
+		Conn: client,
 	}
 
+	donec := make(chan bool, 2)
+	go copySync(idleCbw, idleCbr, donec)
+	go copySync(idleCbr, idleCbw, donec)
+	<-donec
+	<-donec
+
+}
+
+func copySync(w io.Writer, r io.Reader, donec chan<- bool) {
+	if _, err := io.Copy(w, r); err != nil && err != io.EOF {
+		fmt.Printf(" failed to copy : %v\n", err)
+	}
+
+	fmt.Printf(" finished copying\n")
+	donec <- true
 }
 func main() {
 	flag.StringVar(&l, "l", "", "listen host:port")
 	flag.StringVar(&r, "r", "", "remote host:port")
 	flag.Parse()
 	if len(l) <= 0 {
-		flag.PrintDefaults();
+		flag.PrintDefaults()
 		os.Exit(-1)
 	}
 	if len(r) <= 0 {
@@ -63,6 +81,8 @@ func main() {
 	fmt.Println("Listen on:", l)
 	fmt.Println("Forward request to:", r)
 	listener, err := net.Listen("tcp", l)
+
+	fmt.Println("Dial timeout: ", DialTimeout)
 	if err != nil {
 		fmt.Println("Failed to listen on ", l, err)
 		return
