1a3dfcc48c57015f88c1c9c6bcfe9f233bfaf213
cnmade <hufeng1987@gmail.com>
2023-02-21 16:27:13 +0800

    Update idle conn

--- common/idle_conn.go
+++ common/idle_conn.go
@@ -5,47 +5,49 @@ import (
 	"net"
 	"sync"
 	"sync/atomic"
-	"time"
 )
 
 type IdleTimeoutConnV3 struct {
 	sync.Mutex
 	atomic.Int64
-	update func()
-	Conn   net.Conn
-	logger *zap.SugaredLogger
+	update  func()
+	Conn    net.Conn
+	logger  *zap.SugaredLogger
+	Updated chan int
 }
 
 func NewIdleTimeoutConnV3(conn net.Conn, fn func(), logger *zap.SugaredLogger) *IdleTimeoutConnV3 {
 	c := &IdleTimeoutConnV3{
-		Conn:   conn,
-		update: fn,
-		logger: logger,
+		Conn:    conn,
+		update:  fn,
+		logger:  logger,
+		Updated: make(chan int),
 	}
 	return c
 }
 
 func (ic *IdleTimeoutConnV3) Read(buf []byte) (int, error) {
+	select {
+	case ic.Updated <- 1:
+	default:
+	}
 	go ic.UpdateIdleTime()
 	return ic.Conn.Read(buf)
 }
 
 func (ic *IdleTimeoutConnV3) UpdateIdleTime() {
-	if ic.TryLock() {
-		defer ic.Unlock()
-
-		tmpInt := ic.Load()
-		if tmpInt <= 0 || tmpInt < time.Now().UnixMilli() {
-			ic.Store(time.Now().Add(6 * time.Second).UnixMilli())
-			//	log.Infof(" yes , do update because : %v", tmpInt)
-			_ = ic.Conn.SetReadDeadline(time.Now().Add(45 * time.Second))
-			_ = ic.Conn.SetWriteDeadline(time.Now().Add(45 * time.Second))
-			ic.update()
-		}
+	select {
+	case <-ic.Updated:
+		go ic.update()
+	default:
 	}
 }
 
 func (ic *IdleTimeoutConnV3) Write(buf []byte) (int, error) {
+	select {
+	case ic.Updated <- 1:
+	default:
+	}
 	go ic.UpdateIdleTime()
 	return ic.Conn.Write(buf)
 }
